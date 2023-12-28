b6a02fdf4e64252a31f54fcb09a439874da5f955
dt_1562166555 <735156873@qq.com>
2023-12-28 13:43:52 +0800

    新增

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			const updateManager = wx.getUpdateManager()
+			updateManager.onCheckForUpdate(function(res) {
+				if (res.hasUpdate) {
+					updateManager.onUpdateReady(function(ress) {
+						updateManager.applyUpdate()
+					})
+				} else {
+					updateManager.onUpdateFailed(function() {
+					})
+				}
+			})
+		},
+		onShow: function() {
+			this.$isResolve()
+		},
+		onHide: function() {
+			
+		}
+	};
+</script>
+
+<style lang="scss">
+</style>
